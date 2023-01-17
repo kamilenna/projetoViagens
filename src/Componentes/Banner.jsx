bf7416ff2c7a72ebc8a301fab49671e9564295bf
kamilenna <karla.milenna1@gmail.com>
2023-01-17 16:23:51 -0300

    Componente Banner finalizado

--- src/Componentes/Banner.jsx
+++ src/Componentes/Banner.jsx
@@ -1,27 +1,33 @@
 import React from "react";
-import "../styles/Styles.css"
+import "../styles/Banner.css"
 import { FaMapMarkerAlt, FaCalendarAlt } from "react-icons/fa";
 import Journey from "../assets/Journey.png"
 
 export default function Banner() {
     return (
         <div>
-            <section>
-                <div className="img-inicial">
-                    <img src={Journey} alt="" />
-                </div>
-                <div className="mensagem">
-                    <h1>Explore o mundo!</h1>
-                    <p>É hora de viajar, conhecer novos lugares, viver novas experiências.</p>
+            <section className="banner-inicial">
+                <div className="apresentacao">
+                    <div className="img-inicial">
+                        <img src={Journey} alt="" />
+                    </div>
+                    <div className="mensagem">
+                        <h1>Explore o mundo!</h1>
+                        <p>É hora de viajar, conhecer novos lugares, viver novas experiências.</p>
+                    </div>
                 </div>
                 <section className="formulario">
                     <form>
                         <div className="perguntas">
-                            <label for="ilocal"><FaMapMarkerAlt /> Localização</label>
-                            <input type="text" id="ilocal" placeholder="Para onde deseja viajar?" />
+                            <div className="pgt-local">
+                                <label for="ilocal"><FaMapMarkerAlt /> Localização</label>
+                                <input type="text" id="ilocal" placeholder="Para onde deseja viajar?" />
+                            </div>
 
-                            <label for="idata"><FaCalendarAlt /> Data</label>
-                            <input type="date" id="idata" />
+                            <div className="pgt-data">
+                                <label for="idata"><FaCalendarAlt /> Data</label>
+                                <input type="date" id="idata" />
+                            </div>
                         </div>
                         <button>Buscar</button>
                     </form>
